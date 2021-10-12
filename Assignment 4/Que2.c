b1184eb1b1d5533fa1350f163cc5d49691d303b8
Kunal Patrikar <39524164+kunalpat25@users.noreply.github.com>
2021-10-12 12:23:14 +0530

    Added Assignment 4

---
+++ Assignment 4/Que2.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) 
+{
+    MPI_Init(NULL, NULL);
+
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("Process with Rank : %d (out of %d) and Communicator Group : %d\n", 
+         rank, size, MPI_Comm_group);
+
+    MPI_Finalize();
+}
